b2297848d62c0c5c9546962c6e31ee5d4d92907a
Matthew Harkey <matthewharkey@matthews-macbook-air-65.local>
2019-02-27 11:38:00 -0600

    Parse JSON data

--- Chuck Norris Joke Generator/JokesViewController.swift
+++ Chuck Norris Joke Generator/JokesViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class JokesViewController: UITableViewController {
 
+    var jokes = [[String: String]]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Jokes"
+        let query = "http://api.icndb.com/jokes/random/"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["status"] == "success" {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError()
     }
-
-
+    
+    func parse(json: JSON){
+        for result in json["value"].arrayValue {
+            let id = result["id"].stringValue
+            let joke = result["joke"].stringValue
+            let categories = result["categories"].arrayValue
+            let source = ["id": id, "joke": joke, "categories": categories] as [String : Any]
+            jokes.append(source as! [String : String])
+        }
+        tableView.reloadData()
+    }
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the joke",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
