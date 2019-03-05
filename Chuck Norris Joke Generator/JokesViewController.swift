d17a42b6282ac984156d37f830e859393f8c1975
Matthew Harkey <matthewharkey@matthews-macbook-air-65.local>
2019-03-05 11:16:26 -0600

    Finished initial set up of SourcesTableView

--- Chuck Norris Joke Generator/JokesViewController.swift
+++ Chuck Norris Joke Generator/JokesViewController.swift
@@ -46,8 +46,16 @@ class JokesViewController: UITableViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
-    
-    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jokes.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = jokes[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
     
 }
 
